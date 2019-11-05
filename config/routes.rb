f4287c41148059b649cd26f22e388821bbbd4704
aki <akawa450@gmail.com>
2019-11-05 18:56:48 +0900

    add bbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  get 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
-  get 'topics/index'
+  root 'topics/index'
   get 'topics/show/:id' => 'topics#show', as: :topics_show
   post 'topic/create'  => 'topics#create'
   delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
